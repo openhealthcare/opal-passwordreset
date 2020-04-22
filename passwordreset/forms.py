8584ec7b0fab769e0c00e11b9a37eaaf686d79d2
fredkingham <fredkingham@gmail.com>
2020-04-22 10:19:30 +0100

    Change the form to use placeholder text

--- passwordreset/forms.py
+++ passwordreset/forms.py
@@ -4,7 +4,7 @@ from django.contrib.auth import forms
 class OpalPasswordResetForm(forms.PasswordResetForm):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.fields['email'].widget.attrs.update({'class': 'form-control'})
+        self.fields['email'].widget.attrs.update({'class': 'form-control', 'placeholder': 'Email'})
 
 
 class OpalSetPasswordForm(forms.SetPasswordForm):
